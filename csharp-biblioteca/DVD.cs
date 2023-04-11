702af4fdf10c66b75b6463d79242132cb184f652
slivcaigor <slivcaigor@gmail.com>
2023-04-11 15:37:33 +0200

    Classe DVD

---
+++ csharp-biblioteca/DVD.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    // Definizione della classe DVD, che eredita dalla classe Documento
+    class DVD : Documento
+    {
+        // Definizione della proprietà aggiuntiva del DVD
+        public TimeSpan durata;
+
+        // Costruttore della classe DVD
+        public DVD(string codice, string titolo, int anno, string settore, string scaffale, Autore autore, TimeSpan durata)
+            : base(codice, titolo, anno, settore, scaffale, autore)
+        {
+            // Inizializzazione della proprietà aggiuntiva del DVD
+            this.durata = durata;
+        }
+    }
+}
