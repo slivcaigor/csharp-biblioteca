73f44471005b23fe3c958176369ff66d87561cf2
slivcaigor <slivcaigor@gmail.com>
2023-04-11 15:30:21 +0200

    Classe Libro

---
+++ csharp-biblioteca/Libro.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    // Definizione della classe Libro, che eredita dalla classe Documento
+    class Libro : Documento
+    {
+        // Definizione della proprietà aggiuntiva del libro
+        public int pagine;
+
+        // Costruttore della classe Libro
+        public Libro(string codice, string titolo, int anno, string settore, string scaffale, Autore autore, int pagine)
+            : base(codice, titolo, anno, settore, scaffale, autore)
+        {
+            // Inizializzazione della proprietà aggiuntiva del libro
+            this.pagine = pagine;
+        }
+    }
+}
--- csharp-biblioteca/Program.cs
+++ csharp-biblioteca/Program.cs
@@ -20,6 +20,7 @@
 
 
     Per i libri si ha in aggiunta il numero di pagine, mentre per i dvd la durata.
+
     L’utente deve poter eseguire delle ricerche per codice o per titolo e, eventualmente, prendere in prestito registrando il periodo (Dal/Al) del prestito e il documento.
     Deve essere possibile effettuare la ricerca dei prestiti dato nome e cognome di un utente.
     Creiamo anche una classe Biblioteca che contiene la lista dei documenti, la lista degli utenti e la lista dei prestiti. Contiene inoltre i metodi per le ricerche e per l’aggiunta dei documenti, utenti e prestiti.
