5b1988865ed091cdbd14b8ca92890c5b1bb31ee0
slivcaigor <slivcaigor@gmail.com>
2023-04-11 15:40:36 +0200

    Classe Prestito

---
+++ csharp-biblioteca/Prestito.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+
+    // Definizione della classe Prestito
+    class Prestito
+    {
+        // Definizione delle proprietà del prestito
+        public Utente utente;
+        public Documento documento;
+        public DateTime dal;
+        public DateTime al;
+
+        // Costruttore della classe Prestito
+        public Prestito(Utente utente, Documento documento, DateTime dal, DateTime al)
+        {
+            // Inizializzazione delle proprietà del prestito
+            this.utente = utente;
+            this.documento = documento;
+            this.dal = dal;
+            this.al = al;
+        }
+    }
+}
